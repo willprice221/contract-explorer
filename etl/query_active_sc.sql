0bc3411a0a50de84d3e063c999a2737d79ccca67
astudnev <astudnev@gmail.com>
2019-03-09 18:07:27 +0100

    ETL cont'd

--- etl/query_active_sc.sql
+++ etl/query_active_sc.sql
@@ -13,6 +13,13 @@ LIMIT 50000
 INTO OUTFILE 'top_contracts.csv' FORMAT CSV
 
 
-
 SELECT
-  concat('0x',lower(hex(smart_contract_address_bin))) as smart_contract_address,
+  dictGetString('currency', 'address', toUInt64(currency_id)) AS smart_contract_address,
+  count(*) txs
+FROM  production.transfers_currency
+WHERE currency_id!=1
+GROUP BY currency_id
+ORDER BY txs DESC
+LIMIT 10000
+INTO OUTFILE 'top_tokens.csv' FORMAT CSV
+

5412a68958012f6b97149a24194914944313fc66
Alexander Azarov <azarov@osinka.ru>
2019-03-09 23:39:43 +0100

    🚀 listen on all network interfaces

--- web/main.py
+++ web/main.py
@@ -47,4 +47,4 @@ def showFunction(contract_address, function_hash):
         function_sources = function_sources)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)

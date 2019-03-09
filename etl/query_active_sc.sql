dc74138646b275d3c89d8a860a0c02ff8fc8d2ed
astudnev <astudnev@gmail.com>
2019-03-09 18:21:56 +0100

    Long lots of scs

--- etl/query_active_sc.sql
+++ etl/query_active_sc.sql
@@ -2,24 +2,24 @@
 
 
 SELECT
-  concat('0x',lower(hex(smart_contract_address_bin))) as smart_contract_address,
-  uniq(signature_id) functions,
-  count(*) txs
-FROM production.calls_sc
-GROUP BY smart_contract_address_bin
-HAVING functions<50
-ORDER BY functions*txs DESC
-LIMIT 50000
-INTO OUTFILE 'top_contracts.csv' FORMAT CSV
+  DISTINCT smart_contract_address
+FROM (
+  SELECT
+    concat('0x',lower(hex(smart_contract_address_bin))) as smart_contract_address
+  FROM production.calls_sc
+  GROUP BY smart_contract_address_bin
+  HAVING uniq(signature_id)<50
+  ORDER BY uniq(signature_id)*count(*) DESC
+  LIMIT 100000
 
+  UNION ALL
 
-SELECT
-  dictGetString('currency', 'address', toUInt64(currency_id)) AS smart_contract_address,
-  count(*) txs
-FROM  production.transfers_currency
-WHERE currency_id!=1
-GROUP BY currency_id
-ORDER BY txs DESC
-LIMIT 10000
-INTO OUTFILE 'top_tokens.csv' FORMAT CSV
+  SELECT
+    dictGetString('currency', 'address', toUInt64(currency_id)) AS smart_contract_address
+  FROM  production.transfers_currency
+  WHERE currency_id!=1
+  GROUP BY currency_id
+  ORDER BY count(*) DESC
+  LIMIT 50000
+) INTO OUTFILE 'top_tokens_sc2.csv' FORMAT CSV
 
